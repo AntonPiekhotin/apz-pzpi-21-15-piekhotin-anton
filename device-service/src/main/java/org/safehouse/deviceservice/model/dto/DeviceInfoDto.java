fa14d6d299a8316fc15c6a886b2cb2692ba96d02
Anton <114821726+NurePiekhotinAnton@users.noreply.github.com>
2024-05-15 16:42:16 +0300

    Delete device-service directory

--- device-service/src/main/java/org/safehouse/deviceservice/model/dto/DeviceInfoDto.java
+++
@@ -1,9 +0,0 @@
-package org.safehouse.deviceservice.model.dto;
-
-import lombok.Data;
-import org.safehouse.deviceservice.model.entity.DeviceType;
-
-@Data
-public class DeviceInfoDto {
-	DeviceType type;
-}
